4afbea0c7652c9950c06719bcb3c29e0b06efd3b
garbird <testerfortests725@gmail.com>
2024-07-14 21:38:16 -0500

    Add files via upload

---
+++ uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/static/proxy/',
+    bare:'https://bare.benrogo.net',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/public/uv/uv.handler.js',
+    bundle: '/public/uv/uv.bundle.js',
+    config: '/public/uv/uv.config.js',
+    sw: '/public/uv/uv.sw.js',
+};
